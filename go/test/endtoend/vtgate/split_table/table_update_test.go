83572d0e1a1c0509200105856944f215cbf88d68
zhangjunjie64 <zhangjunjie64@jd.com>
2023-10-17 10:37:41 +0800

    add update it tests (#87)
    * add select checks.
    
    * add update it tests.
    
    * Merge remote-tracking branch 'coding/split-table-main' into split-table-main
    
    * trans ExecuteMultiShard to ExecuteBatchMultiShard

---
+++ go/test/endtoend/vtgate/split_table/table_update_test.go
@@ -0,0 +1,68 @@
+package split_table
+
+import (
+	"testing"
+
+	"vitess.io/vitess/go/test/endtoend/utils"
+)
+
+func TestTableBasticUpdate(t *testing.T) {
+	mcmp, closer := start(t)
+	defer closer()
+	mcmp.Exec("use user")
+	mcmp.Exec("insert into t_user(id,col,f_key,f_tinyint,f_bit) values (1, 'a', 'aaa', 1, false),(2, 'b', 'bbb', 2, false),(3, 'c', 'ccc', 3, true)")
+
+	/* sharded tests */
+	// update Scatter-Scatter type
+	execWithColumnCompareAndCheck(mcmp, "UPDATE t_user SET f_key = 'ddd' WHERE f_bit = true", "WHERE f_bit = true", `[[INT64(3) CHAR("c") CHAR("ddd") INT8(3) BIT("\x01")]]`)
+	execWithColumnCompareAndCheck(mcmp, "UPDATE t_user SET f_key = 'eee' WHERE f_tinyint in (1,2)", "WHERE f_tinyint in (1,2)", `[[INT64(1) CHAR("a") CHAR("eee") INT8(1) BIT("\x00")] [INT64(2) CHAR("b") CHAR("eee") INT8(2) BIT("\x00")]]`)
+	// update EqualUnique-Scatter type
+	execWithColumnCompareAndCheck(mcmp, "UPDATE t_user SET f_key = 'fff' WHERE id = 1", "WHERE id = 1", `[[INT64(1) CHAR("a") CHAR("fff") INT8(1) BIT("\x00")]]`)
+	// update Scatter-EqualUnique type
+	execWithColumnCompareAndCheck(mcmp, "UPDATE t_user SET f_key = 'ggg' WHERE col = 'b'", "WHERE col = 'b'", `[[INT64(2) CHAR("b") CHAR("ggg") INT8(2) BIT("\x00")]]`)
+	// update EqualUnique-EqualUnique type
+	execWithColumnCompareAndCheck(mcmp, "UPDATE t_user SET f_key = 'hhh' WHERE id = 3 AND col = 'c'", "WHERE id = 3 AND col = 'c'", `[[INT64(3) CHAR("c") CHAR("hhh") INT8(3) BIT("\x01")]]`)
+	// update IN-Scatter type
+	execWithColumnCompareAndCheck(mcmp, "UPDATE t_user SET f_bit = true WHERE id in (1,2)", "WHERE id in (1,2)", `[[INT64(1) CHAR("a") CHAR("fff") INT8(1) BIT("\x01")] [INT64(2) CHAR("b") CHAR("ggg") INT8(2) BIT("\x01")]]`)
+	// update Scatter-IN type
+	execWithColumnCompareAndCheck(mcmp, "UPDATE t_user SET f_tinyint = 4 WHERE col in ('b','c')", "WHERE col in ('b','c')", `[[INT64(2) CHAR("b") CHAR("ggg") INT8(4) BIT("\x01")] [INT64(3) CHAR("c") CHAR("hhh") INT8(4) BIT("\x01")]]`)
+	// update MultiEqual-MultiEqual type
+	execWithColumnCompareAndCheck(mcmp, "UPDATE t_user SET f_bit = true WHERE (col,f_key) in (('b','ggg'),('c','hhh'))", "WHERE (col,f_key) in (('b','ggg'),('c','hhh'))",
+		`[[INT64(2) CHAR("b") CHAR("ggg") INT8(4) BIT("\x01")] [INT64(3) CHAR("c") CHAR("hhh") INT8(4) BIT("\x01")]]`)
+}
+
+func TestTableDifficultUpdate(t *testing.T) {
+	mcmp, closer := start(t)
+	defer closer()
+	mcmp.Exec("use user")
+	mcmp.Exec("insert into t_user(id,col,f_key,f_tinyint,f_bit) values (1, 'a', 'aaa', 1, false),(2, 'b', 'bbb', 2, false),(3, 'c', 'ccc', 3, true)")
+
+	/* difficult sql tests*/
+	// update Scatter-Scatter type
+	execWithColumnCompareAndCheck(mcmp, "UPDATE t_user AS user SET user.f_key = 'ddd' WHERE user.f_bit = true", "WHERE f_bit = true", `[[INT64(3) CHAR("c") CHAR("ddd") INT8(3) BIT("\x01")]]`)
+	execWithColumnCompareAndCheck(mcmp, "UPDATE t_user AS user SET user.f_key = 'eee' WHERE user.f_tinyint in (1,2)", "WHERE f_tinyint in (1,2)", `[[INT64(1) CHAR("a") CHAR("eee") INT8(1) BIT("\x00")] [INT64(2) CHAR("b") CHAR("eee") INT8(2) BIT("\x00")]]`)
+	// update EqualUnique-Scatter type
+	execWithColumnCompareAndCheck(mcmp, "UPDATE t_user AS user SET user.f_key = 'fff' WHERE user.id = 1", "WHERE id = 1", `[[INT64(1) CHAR("a") CHAR("fff") INT8(1) BIT("\x00")]]`)
+	// update Scatter-EqualUnique type
+	execWithColumnCompareAndCheck(mcmp, "UPDATE t_user AS user SET user.f_key = 'ggg' WHERE user.col = 'b'", "WHERE col = 'b'", `[[INT64(2) CHAR("b") CHAR("ggg") INT8(2) BIT("\x00")]]`)
+	// update EqualUnique-EqualUnique type
+	execWithColumnCompareAndCheck(mcmp, "UPDATE t_user AS user SET user.f_key = 'hhh' WHERE user.id = 3 AND user.col = 'c'", "WHERE id = 3 AND col = 'c'", `[[INT64(3) CHAR("c") CHAR("hhh") INT8(3) BIT("\x01")]]`)
+	// update IN-Scatter type
+	execWithColumnCompareAndCheck(mcmp, "UPDATE t_user AS user SET user.f_bit = true WHERE user.id in (1,2)", "WHERE id in (1,2)", `[[INT64(1) CHAR("a") CHAR("fff") INT8(1) BIT("\x01")] [INT64(2) CHAR("b") CHAR("ggg") INT8(2) BIT("\x01")]]`)
+	// update Scatter-IN type
+	execWithColumnCompareAndCheck(mcmp, "UPDATE t_user AS user SET user.f_tinyint = 4 WHERE user.col in ('b','c')", "WHERE col in ('b','c')", `[[INT64(2) CHAR("b") CHAR("ggg") INT8(4) BIT("\x01")] [INT64(3) CHAR("c") CHAR("hhh") INT8(4) BIT("\x01")]]`)
+	// update MultiEqual-MultiEqual type
+	execWithColumnCompareAndCheck(mcmp, "UPDATE t_user AS user SET user.f_bit = true WHERE (user.col,user.f_key) in (('b','ggg'),('c','hhh'))", "WHERE (col,f_key) in (('b','ggg'),('c','hhh'))",
+		`[[INT64(2) CHAR("b") CHAR("ggg") INT8(4) BIT("\x01")] [INT64(3) CHAR("c") CHAR("hhh") INT8(4) BIT("\x01")]]`)
+
+	execWithColumnCompareAndCheck(mcmp, "UPDATE t_user AS user SET user.f_tinyint = user.f_tinyint + 1 WHERE user.id = 3 AND user.col = 'c'", "WHERE id = 3 AND col = 'c'",
+		`[[INT64(3) CHAR("c") CHAR("hhh") INT8(5) BIT("\x01")]]`)
+	execWithColumnCompareAndCheck(mcmp, "UPDATE t_user AS user SET user.f_bit = false where user.id > 1", "",
+		`[[INT64(1) CHAR("a") CHAR("fff") INT8(1) BIT("\x01")] [INT64(2) CHAR("b") CHAR("ggg") INT8(4) BIT("\x00")] [INT64(3) CHAR("c") CHAR("hhh") INT8(5) BIT("\x00")]]`)
+}
+
+func execWithColumnCompareAndCheck(mcmp utils.MySQLCompare, query string, checkClause string, expected string) {
+	mcmp.ExecWithColumnCompare(query)
+	checkQuery := "SELECT id,col,f_key,f_tinyint,f_bit FROM t_user " + checkClause + " ORDER BY id"
+	mcmp.AssertMatches(checkQuery, expected)
+}
